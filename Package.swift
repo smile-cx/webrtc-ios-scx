8943489b487838591492f5e962bcb995b4ddcdfd
Michael Siddi <michael.siddi@covisian.com>
2026-03-12 15:15:38 +0100

    Add SmileCX prefix patch for iOS WebRTC
    - Adds SCX ObjC prefix for all WebRTC classes
    - Changes framework name to SmileCXWebRTC
    - Updates bundle ID to cx.smile.SmileCXWebRTC
    - Updates workflow to apply patch during build
    - Updates Package.swift for Swift Package Manager
    - Includes verification checks in CI/CD

--- Package.swift
+++ Package.swift
@@ -3,18 +3,18 @@
 import PackageDescription
 
 let package = Package(
-    name: "WebRTC",    
+    name: "SmileCXWebRTC",
     products: [
         .library(
-            name: "WebRTC",
-            targets: ["WebRTC"]),
+            name: "SmileCXWebRTC",
+            targets: ["SmileCXWebRTC"]),
     ],
     dependencies: [],
     targets: [
         .binaryTarget(
-            name: "WebRTC", 
-            url: "https://github.com/alexpiezo/WebRTC/releases/download/95.4638.0/WebRTC-M95.xcframework.zip",
+            name: "SmileCXWebRTC",
+            url: "https://github.com/smile-cx/webrtc-ios-scx/releases/download/95.4638.0/SmileCXWebRTC-M95.xcframework.zip",
             checksum: "b49d47cdbbd7d72b03a340aa1b55591b82824926ada8a54be992addf614c8325"
-        )        
+        )
     ]
 )
